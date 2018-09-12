48f21513ba95baa4f112c7815ee62c2043dded52
Clark Newell <williamcnewell@gmail.com>
2018-09-11 23:38:31 -0600

    trying axios to get data

--- evernym-challenge/src/components/Table.js
+++ evernym-challenge/src/components/Table.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Table.css';
 
-const url = '../../api/data.json'
-
 class Table extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        };
+    state = {
+        repos: []
     }
 
     componentDidMount() {
-
-        fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        // .then(data => this.setState({ data }))
+        axios.get('../../api/data.json')
+            .then(response => {
+                const repos = response.data;
+                this.setState({ repos })
+            })
     }
 
     render() {
         return (
             <div>
                 <p>Table of repos goes here</p>
+                <ul>
+                    { this.state.repos }
+                </ul>
             </div>
         )
     }
